da99b097240c7ae458058638386d673102e42eb4
untari <sastraembun0306@gmail.com>
2021-02-05 16:49:22 +0700

    navigation part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import Home from './HomeComponent';
 import Dishdetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const MenuNavigator = createStackNavigator({
     Menu: { screen: Menu }, 
@@ -21,11 +21,44 @@ const MenuNavigator = createStackNavigator({
     }
 });
 
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+}, { 
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        }
+    }
+});
+
+const MainNavigator = createDrawerNavigator({
+    Home: {
+        screen: HomeNavigator,
+        navigationOption: {
+            title: 'Home',
+            drawerLabel: 'Home'
+        }
+    },
+    Menu: {
+        screen: MenuNavigator,
+        navigationOption: {
+            title: 'Menu',
+            drawerLabel: 'Menu'
+        }
+    }
+}, {
+    drawerBackgroundColor: '#D1C4E9'
+})
+
 class Main extends Component {
   render() {
     return (
         <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-            <MenuNavigator />
+            <MainNavigator />
         </View>
     );
   }
