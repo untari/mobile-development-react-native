71301482831c131aab0e2fce5445315cf69925fa
untari <sastraembun0306@gmail.com>
2021-02-09 15:21:42 +0700

    setting up redux

---
+++ redux/comments.js
@@ -0,0 +1,16 @@
+import * as ActionTypes from './ActionTypes';
+
+export const comments = (state = {
+    isLoading: true,
+    errMess: null,
+    comments: []
+}, action) => {
+    switch(action.type) {
+        case ActionTypes.ADD_COMMENTS;
+            return { ...state, errMess: null, dishes: action.payload};
+        case  ActionTypes.COMMENTS_FAILED:
+            return { ...state, errMess: action.payload, comments: []};
+        default:
+            return state;
+    }
+}
