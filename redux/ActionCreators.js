8b05e91ce2fa7d9a41e57484331025730b04dd8f
untari <sastraembun0306@gmail.com>
2021-02-12 13:13:20 +0700

    delete favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -167,3 +167,8 @@ export const addFavorite = (dishId) => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: dishId
 });
+
+export const deleteFavorite = (dishId) => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: dishId
+});
