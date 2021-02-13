ddd4cffa0cab844867af58ac2eb4cb78214b7e05
untari <sastraembun0306@gmail.com>
2021-02-13 15:24:48 +0700

    gesture part 1

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component }  from 'react';
-import { Text, View, ScollView, FlatList, StyleSheet, Modal, Button } from 'react-native';
+import { Text, View, ScollView, FlatList, StyleSheet, Modal, Button, Alert, PanResponder } from 'react-native';
 import { Card, Icon, Rating, Input } from  'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -19,13 +19,44 @@ const mapDispatchToProps = dispatch => ({
     postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
 });
 
-function RenderDish({
-   dish,
-   favorite,
-   markFavorite,
-   openCommentForm
-})
-{
+function RenderDish(props){
+    const dish = props.dish;
+    
+    const recognizeDrag = ({ moveX, moveY, dx, dy })  => {
+        if ( dx < -200 )
+            return true;
+        else
+            return false;
+    };
+    
+    const panResponder = panResponder.create({
+        onStartShouldSetpanResponder: (e, gestureState) => {
+           return true; 
+        },
+        onPanResponderEnd: (e, gestureState) => {
+            if (recognizeDrag(gestureState))
+                Alert.alert(
+                    'Add to Favorite?',
+                    ' Are you sure you wish to add ' + dish.name + ' to your favorite',
+                    [
+                        {
+                            text: 'Cancel',
+                            onPress: () => console.log('Cancel pressed'),
+                            style: 'cancel'
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () => favorite ? console.log('Already favorite') : markFavorite()
+                        }
+                    ],
+                    {
+                        cancelable: false
+                    }
+                )
+                return true;
+        }
+    });
+    
     if (dish != null) {
         return(
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
@@ -138,7 +169,7 @@ constructor(props) {
                 <Modal 
                     animationType={'slide'}
                     transparent={false}
-                    visible={this.state.showCOmmentForm}
+                    visible={this.state.showCommentForm}
                     onDismiss={() => {thhis.resetCommentForm()}}
                     onRequestClose={() => {this.resetCommentForm()}}>
                 
