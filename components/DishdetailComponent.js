2cc47190695995b275232c4adf40fc0610925698
untari <sastraembun0306@gmail.com>
2021-02-13 16:00:15 +0700

    gesture part 2

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -22,6 +22,9 @@ const mapDispatchToProps = dispatch => ({
 function RenderDish(props){
     const dish = props.dish;
     
+    handleViewRef = ref => this.view = ref;
+    
+    
     const recognizeDrag = ({ moveX, moveY, dx, dy })  => {
         if ( dx < -200 )
             return true;
@@ -32,6 +35,10 @@ function RenderDish(props){
     const panResponder = panResponder.create({
         onStartShouldSetpanResponder: (e, gestureState) => {
            return true; 
+        },
+        onPanResponderGrant: () => {
+            this.view.rubberBand(1000)
+                .then(endState => console.log(endState.finished ? 'finished' : 'cancelled' ));
         },
         onPanResponderEnd: (e, gestureState) => {
             if (recognizeDrag(gestureState))
@@ -59,7 +66,9 @@ function RenderDish(props){
     
     if (dish != null) {
         return(
-            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
+                ref={this.handleViewRef}
+                {...panResponder.panHandlers}>
                 <Card 
                     featuredTitle={dish.name}
                     image={{ uri: baseUrl + dish.image}}>
